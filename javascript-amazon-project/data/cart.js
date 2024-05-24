dc0171c1a2f1113ae3f43ea09becee9a9e3a1a02
mikemuturi <mikeirungumuturi@gmail.com>
2024-05-24 16:08:06 +0300

    Delete product from cart using delete button

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,5 +1,14 @@
-export const cart = [
-    
+export let cart = [
+    {
+        productId:
+        'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+    },
+    {
+        productId: 
+        '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+    }
 ];
 
 
@@ -22,4 +31,16 @@ export function addToCart(productId){
           });
         }
     
+  }
+
+ export function removeFromCart(productId){
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
   }
