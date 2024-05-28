1857ae8ef6196e78afe2c5fa4f777d2e39320742
mikemuturi <mikeirungumuturi@gmail.com>
2024-05-28 05:22:21 +0300

    Last Changes

--- Js/scripts/11-todo-list.js
+++ Js/scripts/11-todo-list.js
@@ -1,57 +1,101 @@
-const todoList = [
-    {
-      name: 'code',
-      dueDate: '2024-12-22'
-    },
-    {
-      name: 'commit',
-      dueDate: '2024-12-22'
-    }
-  ];
-  
-  function renderTodoList() {
-    let todoListHTML = '';
-  
-    for (let i = 0; i < todoList.length; i++) {
-      const todo = todoList[i];
-      const { name, dueDate } = todo;
-  
-      const html = `
-      
+// initializing empty array to store todo list
+const todoList = [];
+
+//initializing/declare variable of  item being edited. array index start from 0
+let editIndex =  0;
+
+function renderTodoList() {
+  // Initialize an empty string to build the HTML content for the todo list.
+  let todoListHTML = '';
+
+  // Loop through each item in the todoList array. to get the current todo item.
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const { name, dueDate } = todo; 
+
+    // Creating a string of HTML for the current todo item.
+    const html = `
       <div>${name}</div>
-      <div> ${dueDate} </div>
-        <button onclick=" 
-          todoList.splice(${i}, 1);
-          renderTodoList();
-        " class = "delete-todo-button">Delete</button>
-      `;
-      todoListHTML += html;
-    }
-  
-    console.log(todoListHTML);
-    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+      <div>${dueDate}</div>
+      <div class="action-buttons">
+        <button class="edit" onclick="
+          loadTodoForEdit(${i});
+        ">Edit</button>
+        <button onclick="
+          todoList.splice(${i}, 1); // Remove the current item from the todo list.
+          renderTodoList(); // Re-render the todo list.
+        " class="delete-todo-button">Delete</button>
+      </div>
+    `;
+   
+    todoListHTML += html;// adding the todo item to the list/array
   }
+  // confirming todo list created on console 
+  console.log(todoListHTML);
+
+//  Moving the todo list from console to todolist
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addOrUpdateTodo() {
+  // this get the user input values using class name
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+// get the item to edit ,update push to the Array
+//reset index of edited item
+  if (editIndex >= 0) {
   
-  function addTodo() {
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
-  
-    const dateInputElement = document.querySelector('.js-due-date-input');
-    const dueDate = dateInputElement.value;
-  
+    console.log(editIndex);
+    todoList[editIndex] = { 
+      name: name,
+      dueDate: dueDate
+    };
+    editIndex = -1;
+  } else {
     todoList.push({
       name: name,
       dueDate: dueDate
     });
-  
-    console.log(todoList);
-  
-    inputElement.value = '';
-    dateInputElement.value = '';
-  
-    renderTodoList();
   }
-  
-  // Initial render
+
+ 
+
+//  Reseting input fields.
+  inputElement.value = '';
+  dateInputElement.value = '';
+
+  // refresh the list to add new todo
   renderTodoList();
+
+  // Reset the button text to 'Add'.
   
+document.querySelector('.add-todo-button').innerText = 'Add';
+
+}
+
+
+// this function get the item to be edited 
+function loadTodoForEdit(index) {
+  const todo = todoList[index];
+  const { name, dueDate } = todo;
+
+// /Here We populate the selected to to the input field
+  const inputElement = document.querySelector('.js-name-input');
+  const dateInputElement = document.querySelector('.js-due-date-input');
+
+  inputElement.value = name;
+  dateInputElement.value = dueDate;
+
+  // assigning editIndex to the todo being edited
+  editIndex = index;
+ 
+  // reusing the add button to save when a todo item is populated for editing
+  document.querySelector('.add-todo-button').innerText = 'Save';
+  
+}
+
+// finally I load the list
+renderTodoList();
--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,80 +1,67 @@
-export let cart = 
- 
-JSON.parse(localStorage.getItem('cart'));
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if(!cart){
-    cart = [
-        {
-            productId:
-            'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            productId: 
-            '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            quantity: 1,
-            deliveryOptionId: '2'
-        }
-    ];
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
 }
 
-
-function saveToStorage(){
-    localStorage.setItem('cart', JSON.stringify(cart));
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
+export function addToCart(productId) {
+  let matchingItem;
 
-export function addToCart(productId){
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-    let matchingItem;
-  
-        cart.forEach((cartItem) => {
-          if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-          }
-        });
-  
-        if (matchingItem) {
-          matchingItem.quantity += 1;
-        } else {
-          cart.push({
-            productId: productId,
-            quantity: 1,
-            deliveryOptionId: '1',
-          });
-        }
-    saveToStorage();
-  }
-
- export function removeFromCart(productId){
-    const newCart = [];
-
-    cart.forEach((cartItem) => {
-        if(cartItem.productId !== productId){
-            newCart.push(cartItem);
-        }
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
     });
-
-    cart = newCart;
-
-    saveToStorage();
   }
 
+  saveToStorage();
+}
 
-  export function updateDeliveryOption(productId, deliveryOptionId){
+export function removeFromCart(productId) {
+  const newCart = [];
 
-    let matchingItem;
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
 
-    cart.forEach((cartItem) => {
-        if(productId === cartItem.productId){
-            matchingItem = cartItem;
-        }
-    });
+  cart = newCart;
 
+  saveToStorage();
+}
 
-    matchingItem.deliveryOptionId = deliveryOptionId;
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
 
-    saveToStorage();
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-  }
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
